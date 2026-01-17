3885a62231594de9ab5d79a05cb07deab7ed5af0
Jakub Wilk <kubawilk63@gmail.com>
2026-01-17 14:29:21 +0100

    Use Gated attention MIL

--- model.py
+++ model.py
@@ -15,7 +15,8 @@ class AttentionMILModel(torch.nn.Module):
 
 
         self.fc1 = torch.nn.Linear(emb_dim, att_dim)
-        self.fc2 = torch.nn.Linear(att_dim, 1)
+        self.fc2 = torch.nn.Linear(emb_dim, att_dim)
+        self.fc3 = torch.nn.Linear(att_dim, 1)
 
         self.classifier = torch.nn.Linear(emb_dim, output_dim)
 
@@ -28,7 +29,7 @@ class AttentionMILModel(torch.nn.Module):
         X = self.resnet(X[mask != 0])  # (batch_size * bag_size, emb_dim)
 
         # Put back the processed instances to their original positions, so that the shape is preserved (as if all instances, including padding, were processed)
-        resnet_output = torch.zeros((batch_size * bag_size, X.shape[1]), device=X.device)
+        resnet_output = torch.zeros((batch_size * bag_size, X.shape[1]), device=X.device, dtype=X.dtype)
         resnet_output[mask != 0] = X
         X = resnet_output
 
@@ -37,7 +38,10 @@ class AttentionMILModel(torch.nn.Module):
         mask = mask.reshape((batch_size, bag_size))  # (batch_size, bag_size)
 
         H = torch.tanh(self.fc1(X))  # (batch_size, bag_size, att_dim)
-        att = torch.sigmoid(self.fc2(H))  # (batch_size, bag_size, 1)
+        att = torch.sigmoid(self.fc2(X))  # (batch_size, bag_size, att_dim)
+
+        att = torch.mul(H, att) # (batch_size, bag_size, att_dim)
+        att = self.fc3(att) # (batch_size, bag_size, 1)
 
         att_s = masked_softmax(att, mask)  # (batch_size, bag_size, 1)
         # att_s = torch.nn.functional.softmax(att, dim=1)
