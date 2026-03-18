c36b69015daf020fb664c823b0f89aaf1d3c49d7
Jakub Wilk <kubawilk63@gmail.com>
2026-03-18 09:17:19 +0100

    Simplify training script

--- train.py
+++ train.py
@@ -3,78 +3,89 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import Sampler
-from torchvision.transforms import v2
 from image_patcher import ImagePatcher
 from dataset import MILDataset
-from metrics import BinaryMetricsCalculator, MulticlassMetricsCalculator
-from argparse import ArgumentParser
+from metrics import BinaryMetricsCalculator
 from ddp_utils import init_distributed, cleanup_distributed, gather_from_ranks
 from data_utils import create_dataloader
 from model_utils import build_model
-import wandb
 from tqdm import tqdm
-import json
 import torch.nn.functional as F
 from time import gmtime, strftime
-import yaml
-from typing import Dict
 import albumentations as A
 import cv2
+import optuna
+from optuna.samplers import TPESampler
+import torch.distributed as dist
+from functools import partial
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+"""
+TODO: HERE IMPORT YOUR DATASET AND MODEL CLASSES
+"""
+from dataset import YourDataset
+from model import YourModelClass
 
 
+SEED = 42
 
-def parse_args():
-    parser = ArgumentParser(description="Train Attention-based MIL Model")
-    parser.add_argument("--data-dir", type=str, required=True, help="Path to the dataset directory")
-    parser.add_argument("--log-wandb", action="store_true", help="Whether to log training to Weights & Biases")
-    parser.add_argument("--class-selection", action="store_true", help="If used classes are defined based on classes.json config file")
-    parser.add_argument("--log-name", type=str, required=False, default=strftime("%Y-%m-%d_%H:%M:%S", gmtime()))
-    return parser.parse_args()
+# Set seeds
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
 
-# Parses train config defined as yaml file
-def parse_train_config() -> Dict:
-    with open("config/train_config.yaml", "r") as f:
-        train_config = yaml.safe_load(f)
-    return train_config
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 
-def get_logger():
-    # Start a new wandb run to track this script.
-    wandb_logger = wandb.init(
-        # Set the wandb entity where your project will be logged (generally your team name).
-        entity="kubawilk63-politechnika-gda-ska",
-        # Set the wandb project where this run will be logged.
-        project="MIL-Breast-Cancer",
-        # Track hyperparameters and run metadata.
-        config=train_config,
-    )
-    return wandb_logger
-    
+# TODO: Change those values
+LOG_NAME = strftime("%Y-%m-%d_%H:%M:%S", gmtime()) # Log name used for saving model and logging to wandb
+NUM_EPOCHS = 5
+NUM_TRIALS = 8
+AVG_METHOD = "macro"  # Averaging method for calculating metrics. Macro, micro or None (to get separate metrics for each class)
+NUM_WORKERS = 16
+BATCH_SIZE = 2
+LR = 1e-4 # Learning rate
+WEIGHT_DECAY = 1e-5 # Weight decay for optimizer
 
 
-def log_metric(logger, metric: torch.Tensor, metric_name: str):
-    assert isinstance(metric, torch.Tensor), f"Expected metric to be torch.Tensor, found {metric_name} of type {type(metric)}"
+def save_first_n_images(dataloader, n=5, save_dir="debug_images"):
+    os.makedirs(save_dir, exist_ok=True)
+    count = 0
+    for batch in dataloader:
+        images = batch[0] if isinstance(batch, (list, tuple)) else batch
+        for i in range(images.size(0)):
+            img = images[i].detach().cpu().numpy()
 
-    if metric.ndim != 0:
-        # Log separate metric for each class
-        for class_id in range(metric.size()[0]):
-            logger.log({f"{metric_name}_{class_id}": metric[class_id]})
-    else:
-        logger.log({metric_name: metric})
+            # If image has shape (C, H, W), transpose to (H, W, C)
+            if img.shape[0] <= 4:
+                img = np.transpose(img, (1, 2, 0))
+                
+            # Min-max normalization
+            img_min = img.min()
+            img_max = img.max()
+            img_norm = (img - img_min) / (img_max - img_min + 1e-8)
+
+            # If single channel, squeeze last dim
+            if img_norm.shape[-1] == 1:
+                img_norm = img_norm.squeeze(-1)
+            
+            plt.imsave(os.path.join(save_dir, f"img_{count+1}.png"), img_norm, cmap='gray' if img_norm.ndim == 2 else None)
+            count += 1
+            if count >= n:
+                return
 
 
 # Given a model and validation dataloader, evaluate the model performance on validation set
-def validate(model, val_dl, criterion, output_dim, is_ddp, rank, world_size, device):
+def validate(model, val_dl, criterion, is_ddp, rank, world_size, device):
     # Initialize validation dataloader with correct number of classes
-    if output_dim == 1:
-        metrics_calculator = BinaryMetricsCalculator()
-    else:
-        metrics_calculator = MulticlassMetricsCalculator(num_classes=output_dim, avg_method=train_config["avg_method"])
+    metrics_calculator = BinaryMetricsCalculator()
 
     val_loss = 0.0 # Track validation loss
     outputs_list = []
     targets_list = []
-    losses_list = []
 
     if rank == 0:
         iterator = tqdm(val_dl, desc="Validation")
@@ -83,29 +94,29 @@ def validate(model, val_dl, criterion, output_dim, is_ddp, rank, world_size, dev
 
     model.eval()
     with torch.no_grad():
-        for features, labels, masks, bags_length, instances_idx, instances_cords in iterator:
+        for inputs, labels in iterator:
             # Move data to device
-            features = features.to(device)
+            inputs = inputs.to(device)
             labels = labels.to(device)
-            masks = masks.to(device)
 
             with torch.autocast(device_type="cuda", dtype=torch.float16):
                 # Model forward pass
-                outputs = model(features, masks, bags_length)
+                logits = model(inputs).squeeze(1)
 
                 # If binary classification use sigmoid and transform labels to float
                 labels = labels.to(torch.float32)
         
-                loss = criterion(outputs, labels)
+                # Criterion is already set to BCEWithLogitsLoss so we are passing logits
+                loss = criterion(logits, labels)
 
-                if output_dim == 1:
-                    outputs = F.sigmoid(outputs) # For metrics calculation
+                # Use sigmoid just for metrics calculation
+                outputs = F.sigmoid(logits)
 
-            losses_list.append(loss.item())
             val_loss += loss.item()
             outputs_list.extend(outputs.detach().cpu().tolist())
             targets_list.extend(labels.detach().cpu().tolist())
 
+    # Gather outputs, targets and losses from all ranks to calculate metrics on the whole validation set
     gathered_outputs = gather_from_ranks(outputs_list, is_ddp, world_size)
     gathered_targets = gather_from_ranks(targets_list, is_ddp, world_size)
     gathered_losses = gather_from_ranks(val_loss, is_ddp, world_size)
@@ -113,12 +124,15 @@ def validate(model, val_dl, criterion, output_dim, is_ddp, rank, world_size, dev
     if rank != 0:
         return None
     
+    # Convert gathered lists to tensors and flatten them
     gathered_losses = torch.tensor(gathered_losses).flatten()
     gathered_outputs = torch.tensor(gathered_outputs).flatten(0, 1)
     gathered_targets = torch.tensor(gathered_targets).flatten(0, 1)
 
+    # Get average validation loss
     avg_val_loss = torch.tensor(gathered_losses.mean() / len(val_dl))
 
+    # Calculate validation metrics
     val_accuracy, val_f1_score, val_auprc, val_auroc, val_precision, val_recall, _ = metrics_calculator.calculate(gathered_outputs, gathered_targets)
     return avg_val_loss, val_accuracy, val_f1_score, val_auprc, val_auroc, val_precision, val_recall, gathered_outputs, gathered_targets
 
@@ -132,21 +146,16 @@ def train(model: torch.nn.Module,
           optimizer: torch.optim.Optimizer, 
           device: str, 
           num_epochs: int, 
-          output_dim: int, 
           is_ddp: bool, 
           rank: int, 
           world_size: int, 
-          log_name: str, 
-          logger = None):
+          log_name: str):
     # Initialize variables to track best model
     best_val_auprc = 0.0
     best_weights = model.state_dict()
     
     # Use correct metrics calculator for classification problem
-    if output_dim == 1:
-        metrics_calculator = BinaryMetricsCalculator()
-    else:
-        metrics_calculator = MulticlassMetricsCalculator(num_classes=output_dim, avg_method=train_config["avg_method"])
+    metrics_calculator = BinaryMetricsCalculator()
     
     for epoch in range(num_epochs):
         if rank == 0:
@@ -166,24 +175,25 @@ def train(model: torch.nn.Module,
         scaler = torch.amp.GradScaler()
 
         model.train()
-        for features, labels, masks, bags_length, instances_idx, instances_cords in iterator:
+        for inputs, labels in iterator:
             optimizer.zero_grad() # Zero the gradients
 
             # Move data to device
-            features = features.to(device)
-            masks = masks.to(device)
+            inputs = inputs.to(device)
             labels = labels.to(device)
 
             with torch.autocast(device_type="cuda", dtype=torch.float16):
                 # Model and criterion forward pass
-                outputs = model(features, masks, bags_length)
+                logits = model(inputs).squeeze(1)
 
                 labels = labels.to(torch.float32)
+                
+                # Criterion is already set to BCEWithLogitsLoss so we are passing logits
+                loss = criterion(logits, labels)
 
-                loss = criterion(outputs, labels)
+                # Use sigmoid just for metrics calculation
+                outputs = F.sigmoid(logits)
 
-                if output_dim == 1:
-                    outputs = F.sigmoid(outputs) # For metrics calculation
 
             # Model optimization step
             scaler.scale(loss).backward()
@@ -203,7 +213,6 @@ def train(model: torch.nn.Module,
             model, 
             val_dl, 
             criterion,
-            output_dim=output_dim,
             is_ddp=is_ddp,
             rank=rank,
             world_size=world_size,
@@ -213,63 +222,22 @@ def train(model: torch.nn.Module,
             avg_val_loss, val_accuracy, val_f1_score, val_auprc, val_auroc, val_precision, val_recall, val_outputs, val_targets = res
 
             # Print epoch summary
-            print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}")
-        
-            # Logging to wandb
-            if logger is not None:
-                log_metric(logger, avg_train_loss, "train_loss")
-                log_metric(logger, train_accuracy, "train_accuracy")
-                log_metric(logger, train_f1_score, "train_f1_score")
-                log_metric(logger, train_auprc, "train_auprc")
-                log_metric(logger, train_auroc, "train_auroc")
-                log_metric(logger, train_precision, "train_precision")
-                log_metric(logger, train_recall, "train_recall")
-                log_metric(logger, avg_val_loss, "val_loss")
-                log_metric(logger, val_accuracy, "val_accuracy")
-                log_metric(logger, val_f1_score, "val_f1_score")
-                log_metric(logger, val_auprc, "val_auprc")
-                log_metric(logger, val_auroc, "val_auroc")
-                log_metric(logger, val_precision, "val_precision")
-                log_metric(logger, val_recall, "val_recall")
-
-                if output_dim == 1:
-                    # Log train and validation confusion matrices
-                    logger.log({"best_val_conf_mat" : wandb.plot.confusion_matrix(probs=None,
-                            y_true=val_targets.tolist(), preds=(val_outputs > 0.5).tolist(),
-                            class_names=["0", "1"], title="Validation confusion matrix")})
-                    
-                    logger.log({"best_train_conf_mat" : wandb.plot.confusion_matrix(probs=None,
-                            y_true=targets_list, preds=(torch.tensor(outputs_list) > 0.5).tolist(),
-                            class_names=["0", "1"], title="Training confusion matrix")})
-
-                else:
-                    # Log train and validation confusion matrices
-                    logger.log({"best_val_conf_mat" : wandb.plot.confusion_matrix(
-                            y_true=val_targets.tolist(), probs=val_outputs.tolist(),
-                            class_names=["0", "1"], title="Validation confusion matrix")})
-                    
-                    logger.log({"best_train_conf_mat" : wandb.plot.confusion_matrix(
-                            y_true=targets_list, probs=outputs_list,
-                            class_names=["0", "1"], title="Training confusion matrix")})
+            print(f"Epoch [{epoch+1}/{num_epochs}]")
+            print(f"\tTrain Loss: {avg_train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, Train F1 Score: {train_f1_score:.4f}, Train AUPRC: {train_auprc:.4f}, Train AUROC: {train_auroc:.4f}, Train Precision: {train_precision:.4f}, Train Recall: {train_recall:.4f}")
+            print(f"\tVal Loss: {avg_val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}, Val F1 Score: {val_f1_score:.4f}, Val AUPRC: {val_auprc:.4f}, Val AUROC: {val_auroc:.4f}, Val Precision: {val_precision:.4f}, Val Recall: {val_recall:.4f}")
 
             if val_auprc > best_val_auprc:
                 best_val_auprc = val_auprc
-                torch.save(model.state_dict(), f"{log_name}_best_attention_mil_model.pth")
+                torch.save(model.state_dict(), f"{log_name}_best.pth")
                 best_weights = model.state_dict()
 
 
     print("Model training complete and saved.")
     model.load_state_dict(best_weights)
-    torch.save(model.state_dict(), f"{log_name}_attention_mil_model.pth")
-    
-    if logger is not None:
-        logger.log_model(path=f"{log_name}_attention_mil_model.pth", name="final_attention_mil_model")
+    torch.save(model.state_dict(), f"{log_name}_last.pth")
 
     return best_val_auprc
 
-
-args = parse_args()
-train_config = parse_train_config()
 
 def main():
     # Setup distributed data processing
@@ -279,23 +247,16 @@ def main():
         print(f"DDP initialized: is_ddp={is_ddp}, world_size={world_size}")
         print(f"Available GPUs: {torch.cuda.device_count()}")
 
-    if args.log_wandb and rank == 0 and is_ddp:     # If distributed, only log from rank 0
-        wandb_logger = get_logger()
-        wandb_logger.log_model(path="model.py", name="attention_mil_model")
-    elif args.log_wandb and not is_ddp:    # Non-distributed logging
-        wandb_logger = get_logger()
-        wandb_logger.log_model(path="model.py", name="attention_mil_model")
-    else:
-        wandb_logger = None
-
-    device = train_config["device"]
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Define image transformations
     val_transform = A.Compose([
+        A.Resize(224, 224),
         A.ToTensorV2(),
     ])
     
     train_transform = A.Compose([
+        A.Resize(224, 224),
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
         A.RandomBrightnessContrast(
@@ -347,53 +308,30 @@ def main():
             fill=0,
             p=0.3,
         ),
-    ])
+    ], seed=SEED)
 
-    # Create patcher used for splitting images into patches
-    patcher = ImagePatcher(patch_size=train_config["patch_size"], overlap=train_config["overlap"])
 
-    # Select subset of classes
-    if args.class_selection:
-        with open("config/classes.json", "r") as f:
-            selected_classes = json.load(f)
-    else:
-        selected_classes = None
+    # TODO: Those values are just an example
+    your_train_args = "data/train_split.csv"
+    your_val_args = "data/val_split.csv"
+    your_model_args = []
 
     # Create dataset and dataloader
-    train_dataset = MILDataset(dataset_csv=os.path.join(args.data_dir, "train_split.csv"), image_patcher=patcher, dirs_with_classes=selected_classes, transform=train_transform)
-    train_dataloader, train_sampler = create_dataloader(train_dataset, batch_size=train_config["batch_size"], shuffle=True, sample_type=train_config["sample_type"], num_workers=train_config["num_workers"], is_ddp=is_ddp, rank=rank, world_size=world_size)
+    train_dataset = YourDataset(your_train_args, transform=train_transform)
+    train_dataloader, train_sampler = create_dataloader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, is_ddp=is_ddp, rank=rank, world_size=world_size, seed=SEED)
 
-    val_dataset = MILDataset(dataset_csv=os.path.join(args.data_dir, "val_split.csv"), image_patcher=patcher, dirs_with_classes=selected_classes, transform=val_transform)
-    val_dataloader, val_sampler = create_dataloader(val_dataset, batch_size=train_config["batch_size"], shuffle=False, sample_type=None, num_workers=train_config["num_workers"], is_ddp=is_ddp, rank=rank, world_size=world_size)
+    val_dataset = YourDataset(your_val_args, transform=val_transform)
+    val_dataloader, val_sampler = create_dataloader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS, is_ddp=is_ddp, rank=rank, world_size=world_size, seed=SEED)
 
-
-    n_classes = len(train_dataset.classes)
-
-    if train_config["output_dim"] == -1:
-        if n_classes == 2:
-            output_dim = 1
-        else:
-            output_dim = n_classes
-    else:
-        output_dim = train_config["output_dim"]
+    save_first_n_images(train_dataloader, n=5, save_dir=f"train_images_rank{rank}")
+    save_first_n_images(val_dataloader, n=5, save_dir=f"val_images_rank{rank}")
 
     # Initialize model, loss function, and optimizer
-    model = build_model(output_dim=output_dim, att_dim=train_config["attention_dim"], dropout_rate=train_config["dropout_rate"], is_ddp=is_ddp, rank=rank, local_rank=local_rank, device=device)
+    model = build_model(YourModelClass, your_model_args, is_ddp=is_ddp, rank=rank, local_rank=local_rank, device=device)
 
-    # Use correct criterion for binary/multiclass classification problem
-    if output_dim == 1:
-        criterion = torch.nn.BCEWithLogitsLoss()
-    else:
-        criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.BCEWithLogitsLoss()
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=train_config["lr"], weight_decay=train_config["weight_decay"])
-
-    # Log additional params to wandb logger
-    if wandb_logger is not None:
-        wandb_logger.config["classes"] = train_dataset.classes
-        wandb_logger.config["optimizer"] = type(optimizer)
-        wandb_logger.config["is_ddp"] = is_ddp
-        wandb_logger.config["world_size"] = world_size
+    optimizer = torch.optim.AdamW(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
 
     # Train the model
     best_val_auprc = train(
@@ -404,18 +342,15 @@ def main():
         criterion, 
         optimizer, 
         device, 
-        num_epochs=train_config["num_epochs"],
-        output_dim=output_dim,
+        num_epochs=NUM_EPOCHS,
         is_ddp=is_ddp,
         rank=rank,
         world_size=world_size, 
-        logger=wandb_logger,
-        log_name=args.log_name)
-
+        log_name=LOG_NAME)
+    
     # Distributed data processing cleanup
     if is_ddp:
         cleanup_distributed()
-
 
 if __name__ == "__main__":
     main()
